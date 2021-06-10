7c438987b6c3302e488f0367f0f09f52a2d01231
Arthur Carcano <arthur.carcano@inria.fr>
2021-06-10 17:57:08 +0200

    remove old -sys crate

--- cvode-5-sys/src/include.h
+++
@@ -1,5 +0,0 @@
-#include <cvode/cvode.h>
-#include <cvodes/cvodes.h>
-#include <nvector/nvector_serial.h>
-#include <sunlinsol/sunlinsol_dense.h>
-#include <sunmatrix/sunmatrix_dense.h>
--- cvode-5-sys/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![allow(
-    non_upper_case_globals,
-    non_camel_case_types,
-    non_snake_case,
-    improper_ctypes,
-    clippy::all
-)]
-
-include!(concat!(env!("OUT_DIR"), "/generated.rs"));
