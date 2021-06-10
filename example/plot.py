29f503861f87798fb73d204a1ecde177505a0870
Arthur Carcano <arthur.carcano@inria.fr>
2021-06-10 15:13:01 +0200

    fix lint in python plot script

--- example/plot.py
+++ example/plot.py
@@ -4,5 +4,5 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv(sys.stdin,names=['t','x',r'\dot{x}',r"dx_dx0", r"d\dot{x}_dx0", r"dx_d\dot{x}0", r"d\dot{x}_d\dot{x}0", r"dx_dk", r"d\dot{x}_dk"],index_col='t')
 ax = df.plot(subplots=True)
-plt.suptitle("\dotdot{x} = -k*x")
+plt.suptitle(r"\dotdot{x} = -k*x")
 plt.show()
